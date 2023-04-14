13eafd95957dacaba42aab526385fcc57a5b137c
jason <jkercher@rlcsystems.com>
2023-04-14 16:08:15 -0400

    fifo

--- fifo.h
+++ fifo.h
@@ -1,51 +1,36 @@
-/* ========================================
- * project: RLC294 -- Topcon Medical Systems 2200/2300/2400 Chair Controller
- * File:    Fifo.h
- *
- * First In First Out circular buffer
- *
- * Rick Kundrat & Zach Martin & Jason Kercher
- * Initial version: 6/7/2022, JASON KERCHER
- *
- * Copyright RLC Electronic Systems, 2022
- * All Rights Reserved
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- *
- * ========================================
-*/
-
 #ifndef FIFO_H
 #define FIFO_H
 
-#include <stdint.h>
+#include <stdatomic.h>
+#include "types.h"
 
-
-/* Use anonymous struct or typedef
- * NOTE: set LEN_ to a power of 2
+/* NOTE: This header uses "statement expresions"
+ *       and typeof which are GNU extensions.
  */
-#define Fifo(T_, LEN_)              \
-	struct {                    \
-		uint16_t head;      \
-		uint16_t tail;      \
-		T_       buf[LEN_]; \
-	}
 
-typedef Fifo(uint8_t, 64) Comms_Fifo;
+/* Use anonymous struct or typedef */
+#define Fifo(T_, LEN_)                 \
+	struct {                       \
+		_Atomic u16 head;      \
+		_Atomic u16 tail;      \
+		T_          buf[LEN_]; \
+	}
 
 #define fifo_len(F_)                 (sizeof((F_).buf) / sizeof((F_).buf[0]))
 #define FIFO_IDX_ADD(F_, IDX_, OFF_) IDX_ = (IDX_ + (OFF_)) % fifo_len(F_)
 
-/* access: These "functions" operate directly on the struct 
+/* access: These "functions" operate directly on the struct
  *         because there is no modification.
  */
-#define fifo_available(F_)                               \
-	({                                               \
-		unsigned avail_ = (F_).head - (F_).tail; \
-		if (avail_ > fifo_len(F_)) {             \
-			avail_ += fifo_len(F_);          \
-		}                                        \
-		avail_;                                  \
+#define fifo_available(F_)                           \
+	({                                           \
+		uint avail_ = (F_).head - (F_).tail; \
+		if (avail_ > fifo_len(F_)) {         \
+			avail_ += fifo_len(F_);      \
+		}                                    \
+		avail_;                              \
 	})
+
 #define fifo_is_empty(F_) ((F_).head == (F_).tail)
 #define fifo_is_full(F_)  (((F_).head + 1) % fifo_len(F_) == (F_).tail)
 
@@ -56,30 +41,21 @@ typedef Fifo(uint8_t, 64) Comms_Fifo;
 /* mutate: These "functions" take a pointer to the struct in order
  *         to signal to the user there are mutations here.
  */
-#define fifo_clear(F_) (F_)->head = (F_)->tail
+#define fifo_clear(F_) (F_)->tail = (F_)->head
 
 #define fifo_consume(F_, OFF_) FIFO_IDX_ADD(*(F_), (F_)->tail, OFF_)
-#define fifo_get(F_)                        \
-	({                                  \
-		unsigned old_ = (F_)->tail; \
-		fifo_consume(F_, 1);        \
-		(F_)->buf[old_];            \
+#define fifo_get(F_)                                               \
+	({                                                         \
+		typeof((F_)->buf[0]) res_ = (F_)->buf[(F_)->tail]; \
+		fifo_consume(F_, 1);                               \
+		res_;                                              \
 	})
 
 #define fifo_advance(F_, OFF_) FIFO_IDX_ADD(*(F_), (F_)->head, OFF_)
 #define fifo_add(F_, ITEM_)                    \
 	{                                      \
-		if (fifo_is_full(*(F_))) {     \
-			fifo_consume(F_, 1);   \
-		}                              \
 		(F_)->buf[(F_)->head] = ITEM_; \
 		fifo_advance(F_, 1);           \
-	}
-#define fifo_add_array(F_, ARR_, LEN_)                     \
-	{                                                  \
-		for (int i_ = 0; i_ < (int)(LEN_); ++i_) { \
-			fifo_add(F_, (ARR_)[i_]);          \
-		}                                          \
 	}
 
 #endif /* FIFO_H */
